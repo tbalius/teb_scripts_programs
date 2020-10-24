26b029ed490b34fe038e8d2270902781224534d8
Trent Balius <baliuste@fr-w-s097455.ncifcrf.gov>
2020-10-24 12:39:10 -0400

    added in a keepH flag.

--- zzz.scripts/chimera_dockprep.py
+++ zzz.scripts/chimera_dockprep.py
@@ -10,9 +10,9 @@ from DockPrep import prep
 ## http://mailman.docking.org/pipermail/dock-fans/2007-May/001043.html
 def main():
   print sys.argv
-  if len(sys.argv) != 3: # if no input
+  if len(sys.argv) != 3 and len(sys.argv) != 4: # if no input
      print "ERROR: Wrong number of inputs"
-     print "syntax: chimera --nogui --script \"chimera_dockprep.py pdbinput output_prefix\""
+     print "syntax: chimera --nogui --script \"chimera_dockprep.py pdbinput output_prefix keepH\""
      print len(sys.argv)
      return
 
@@ -20,6 +20,17 @@ def main():
 
   input_pdb     = sys.argv[1]
   output_prefix = sys.argv[2]
+  if len(sys.argv) == 4:
+      if (sys.argv[3] == 'yes'):
+          keepH = True
+      elif (sys.argv[3] == 'no'): 
+          keepH = False
+      else: 
+          print("keepH must be yes or no, setting it to no.")
+          keepH = False
+
+  else:
+      keepH = False
 
   print "pdb input = " + input_pdb 
   print "output prefix = " + output_prefix
@@ -28,7 +39,8 @@ def main():
   #models = chimera.openModels.list(modelTypes=[chimera.Molecule])
   #model = models
   model = openModels.open(input_pdb)
-  runCommand("del @H,H?,H??,H???") # remove all hydrogens
+  if !(keepH):
+     runCommand("del @H,H?,H??,H???") # remove all hydrogens
   
   writeMol2(model, output_prefix+"before_dockprep.mol2")
 
