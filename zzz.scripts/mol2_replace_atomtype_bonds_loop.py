2ad8d4996a9c4cae2ed1c1a2e9409c12e2c16c5b
Balius <baliuste@fr-w-s097455.ncifcrf.gov>
2021-07-15 15:36:57 -0400

    modifed script so user can change the atom name or leave it unchanged.

--- zzz.scripts/mol2_replace_atomtype_bonds_loop.py
+++ zzz.scripts/mol2_replace_atomtype_bonds_loop.py
@@ -6,7 +6,7 @@ import sys
 # it searches for a bond that formed with an atom that has the search type (atom_t_search)
 # check if its partner atom has the atomtype (atom_t_ori) that needs to be replace then replace with the new type (atom_t_replac)
 
-def loop_bonds(atom_t_ori,atom_t_replace,atom_t_search,mol):
+def loop_bonds(atom_t_ori,atom_t_replace,atom_t_search,atom_name_replace,mol):
 
     for bond in mol.bond_list:
         if (mol.atom_list[bond.a1_num-1].type == atom_t_search):
@@ -14,6 +14,8 @@ def loop_bonds(atom_t_ori,atom_t_replace,atom_t_search,mol):
            print (mol.atom_list[bond.a1_num-1].type, mol.atom_list[bond.a2_num-1].type)
            if (mol.atom_list[bond.a2_num-1].type == atom_t_ori):
                 mol.atom_list[bond.a2_num-1].type = atom_t_replace
+                if (atom_name_replace != "NONE"):
+                   mol.atom_list[bond.a2_num-1].name = atom_name_replace
                 print ("replace")
                 #print (bond.a1_num, bond.a2_num)
                 #print (mol.atom_list[bond.a1_num-1].type, mol.atom_list[bond.a2_num-1].type)
@@ -22,6 +24,8 @@ def loop_bonds(atom_t_ori,atom_t_replace,atom_t_search,mol):
            print (mol.atom_list[bond.a1_num-1].type, mol.atom_list[bond.a2_num-1].type)
            if (mol.atom_list[bond.a1_num-1].type == atom_t_ori):
                 mol.atom_list[bond.a1_num-1].type = atom_t_replace
+                if (atom_name_replace != "NONE"):
+                   mol.atom_list[bond.a1_num-1].name = atom_name_replace
                 print ("replace")
                 #print (bond.a1_num, bond.a2_num)
                 #print (mol.atom_list[bond.a1_num-1].type, mol.atom_list[bond.a2_num-1].type)
@@ -30,7 +34,12 @@ def loop_bonds(atom_t_ori,atom_t_replace,atom_t_search,mol):
     return mol 
 print ("this file requiers the mol2 libary writen by trent balius and sudipto mukherjee")
 
-print ("syntex: mol2_replace_atomtype_bonds.py input_file output_file, a1_ori,a2_rep,a3_search")
+print ("syntex: mol2_replace_atomtype_bonds.py input_file output_file, a1_ori,a2_rep,a3_search, [a2_name] ")
+
+if (len(sys.argv) != 6 or len(sys.argv) != 5):
+   print ("wrong number of arguments. there must be 5 or 6. "
+   exit()
+
 
 infile = sys.argv[1]
 outfile = sys.argv[2]
@@ -38,13 +47,18 @@ a1_ori = sys.argv[3] # ori
 a2_rep = sys.argv[4]
 a3_search = sys.argv[5]
 
-print('infile=%s,\noutfile=%s,\na1_ori=%s,\na2_rep=%s,\na3_search=%s\n'%(infile,outfile,a1_ori,a2_rep,a3_search))
+if (len(sys.argv) == 6):
+  a2_name   = sys.argv[6]  #new atom name
+else: 
+  a2_name = "NONE"
+
+print('infile=%s,\noutfile=%s,\na1_ori=%s,\na2_rep=%s,\na3_search=%s\na2_name=%s\n'%(infile,outfile,a1_ori,a2_rep,a3_search,a2_name))
 
 mol_list = mol2.read_Mol2_file(infile)
 print (len(mol_list))
 count = 0
 for mol in mol_list:
-    moln = loop_bonds( a1_ori,a2_rep,a3_search,mol )
+    moln = loop_bonds( a1_ori,a2_rep,a3_search,a2_name,mol )
     if (count == 0): 
         mol2.write_mol2(moln,outfile)
     else: 
