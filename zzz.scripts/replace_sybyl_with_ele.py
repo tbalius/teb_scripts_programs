4296010e6a0c9c59eda4ad0eae6f5cf223a69c41
Balius <baliuste@fsitgl-head01p.ncifcrf.gov>
2026-07-09 14:49:30 -0400

    updated replace_sybyl_with_ele.py to use the mol2 library. this solves some bugs

--- zzz.scripts/replace_sybyl_with_ele.py
+++ zzz.scripts/replace_sybyl_with_ele.py
@@ -5,9 +5,12 @@
 # molecule has a different atom type for an atom than your docked pose the RMSD correctly. 
 # DOCK 6 uses the Hungarian Algorithm to creat a correspondance amoung poses.  
 
+# Trent updated on 2026/07/09 to use mol2 libary. this solves some bugs.
+
 
 import math, sys
 import os.path
+import mol2_python3 as mol2
 
 from math import sqrt
 
@@ -21,41 +24,6 @@ def sybyl_to_ele(atom_type):
     return ele
 
 
-# read in a mol2 file and replace atom type with the element name. 
-def read_mol2(filename,text1):
-    file1 = open(filename,'r')
-    lines  =  file1.readlines()
-
-    atom_flag = False;
-
-    namestart = 47
-    namestop  = 51
-
-    #namestart = 42
-    #namestop  = 46
-
-    for line in lines:
-         linesplit = line.split() #split on white space
-         if (len(linesplit) > 0):
-             if (linesplit[0] == "@<TRIPOS>BOND"):
-                atom_flag = False
-             if (atom_flag):
-                #atom_type = line[47:51] 
-                atom_type = line[namestart:namestop] 
-                print (atom_type)
-                ele = sybyl_to_ele(atom_type) 
-                #line_mod = '%s%-5s%s\n'%(line[0:47],ele,line[52:-1])
-                line_mod = '%s%-5s%s\n'%(line[0:namestart],ele,line[namestop+1:-1])
-                #text1 = text1+line
-                text1 = text1+line_mod
-             elif (not atom_flag):
-                text1 = text1+line # add line to text1 for output
-             if ( linesplit[0] == "@<TRIPOS>ATOM" ):
-                atom_flag =  True
- 
-
-    file1.close()
-    return text1
 #################################################################################################################
 #################################################################################################################
 def main():
@@ -67,12 +35,14 @@ def main():
     filename    = sys.argv[1]
     output      = sys.argv[2]
 
-    text1 = '' # text to be writen to output
-    text1 = read_mol2(filename,text1)
+    mols = mol2.read_Mol2_file(filename)
+    print("num = %d"%len(mols))
+    for mol in mols:
+        for atom in mol.atom_list:
+            atom.type = sybyl_to_ele(atom.type) 
 
-    file2 = open(output,'w')
-    file2.write(text1)
-    file2.close()
+    #file2 = open(output,'w')
+        mol2.append_mol2(mol,output)
 
     return; 
 #################################################################################################################
